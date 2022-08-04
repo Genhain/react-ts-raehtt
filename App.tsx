2f6f9b684eaee08142e19b3c03d1cb28d87bb37e
Genhain <voxhavoccanis@gmail.com>
2022-08-05 01:04:11 +0800

    1 file modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,49 +1,9 @@
 import * as React from 'react';
 import './style.css';
-import { searchBeers } from './beers';
 import { useState } from "react";
-
 import { Grid, List, TextField, ListItem   } from '@mui/material';
-import { switchMap, concatMap, defer, exhaustMap, fromEvent, map, mergeMap, Observable, of, share, tap, Subscription, from, filter, distinctUntilChanged, debounceTime, observable } from 'rxjs';
 import { JellyfishSpinner } from 'react-spinners-kit';
-
-const style = {
-  mt: 2,
-  ml: 2,
-  width: { sm: 200, md: 300 },
-  backgroundColor: { xs: "aliceblue" },
-  boxShadow: 6
-};
-
-const input$ = defer(() => fromEvent(document.getElementById('input')!, 'keyup').pipe(map(x => (x.target as HTMLInputElement).value)))
-
-const searchBeers$ = (term: string) => new Observable<string[]>(obs => {
-  searchBeers(term, result => {
-    obs.next(result)
-    obs.complete()
-  })
-  
-  return () => console.log("clean up")
-})
-
-const typeAheadFilteredText$ = input$.pipe(
-  debounceTime(500),
-  distinctUntilChanged(),
-  filter(x => x.length > 0 ),
-  share()
-)
-
-const searchedBeers$ = typeAheadFilteredText$.pipe(switchMap(searchBeers$))
-const hasBeganSearching$ = typeAheadFilteredText$.pipe(map(() => true))
-const hasFinishedSearching$ = searchedBeers$.pipe(map(() => false))
-const emptySearch$ = input$.pipe(filter(x => x.length === 0), map(() => []))
-
-const useObservable = (observable: Observable<any>, setter: React.Dispatch<React.SetStateAction<any>>) => {
-  React.useEffect(() => {   
-    let sub = observable.subscribe(setter)
-    return () => sub.unsubscribe()
-  }, [observable, setter])
-}
+import { emptySearch$, hasBeganSearching$, hasFinishedSearching$, searchedBeers$, useObservable } from './observables';
 
 export default function App() {
 
@@ -83,4 +43,12 @@ export default function App() {
     </Grid>
   );
 }
+
+const style = {
+  mt: 2,
+  ml: 2,
+  width: { sm: 200, md: 300 },
+  backgroundColor: { xs: "aliceblue" },
+  boxShadow: 6
+};
 
---
+++ observables.ts
@@ -0,0 +1,33 @@
+import React=require("react")
+import { debounceTime, defer, distinctUntilChanged, filter, fromEvent, map, Observable, share, switchMap } from "rxjs"
+import { searchBeers } from "./beers"
+
+export const input$ = defer(() => fromEvent(document.getElementById('input')!, 'keyup').pipe(map(x => (x.target as HTMLInputElement).value)))
+
+export const searchBeers$ = (term: string) => new Observable<string[]>(obs => {
+  searchBeers(term, result => {
+    obs.next(result)
+    obs.complete()
+  })
+  
+  return () => console.log("clean up")
+})
+
+export const typeAheadFilteredText$ = input$.pipe(
+  debounceTime(500),
+  distinctUntilChanged(),
+  filter(x => x.length > 0 ),
+  share()
+)
+
+export const searchedBeers$ = typeAheadFilteredText$.pipe(switchMap(searchBeers$))
+export const hasBeganSearching$ = typeAheadFilteredText$.pipe(map(() => true))
+export const hasFinishedSearching$ = searchedBeers$.pipe(map(() => false))
+export const emptySearch$ = input$.pipe(filter(x => x.length === 0), map(() => []))
+
+export const useObservable = (observable: Observable<any>, setter: React.Dispatch<React.SetStateAction<any>>) => {
+  React.useEffect(() => {   
+    let sub = observable.subscribe(setter)
+    return () => sub.unsubscribe()
+  }, [observable, setter])
+}
